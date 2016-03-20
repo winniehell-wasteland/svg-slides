52b62940b8872b1a22b290b0257cef4f1a0582da
winniehell <git@winniehell.de>
2016-03-20 18:28:12 +0100

    let SvgSlides implement EventListener and handle all events

--- svg-slides.js
+++ svg-slides.js
@@ -16,142 +16,6 @@ class SvgSlides {
 
       self._options[key] = value;
     });
-
-    console.log('Powered by d3 v' + d3.version);
-    this._rootNode = d3.select(this.options.rootNodeSelector);
-    this.rootNode.attr('preserveAspectRatio', 'xMidYMid meet');
-    this.rootNode.attr('width', '100%');
-    this.rootNode.attr('height', '100%');
-
-    this._slides = this.rootNode.selectAll(this.options.slideSelector)[0];
-
-    this._slides = this._slides.sort(function compareSlides (a, b) {
-      var sortKeyA = a[self.options.sortSlidesBy].toString();
-      var sortKeyB = b[self.options.sortSlidesBy].toString();
-      if (sortKeyA < sortKeyB) {
-        return -1;
-      } else if (sortKeyA > sortKeyB) {
-        return 1;
-      } else {
-        return 0;
-      }
-    });
-
-    if (this.slides.length > 0) {
-      var slideIds = this.slides.map(function (slide) {
-        return slide.id;
-      });
-      console.log(`Found the following slides: ${slideIds}`);
-
-      slideIds.forEach(function (slideId) {
-        var matchingSlides = self.slides.filter(function (slide) {
-          return (slide.id === slideId);
-        });
-        if (matchingSlides.length > 1) {
-          console.error(`Found duplicate slide id: ${slide.id}`);
-        }
-      });
-    } else {
-      console.error('Found no slides!');
-    }
-
-    this.slides.forEach(function (slide) {
-      slide.addEventListener('click', onClickSlide);
-
-      function onClickSlide (event) {
-        self.currentSlideId = slide.id;
-      }
-    });
-
-    window.addEventListener('keydown', onKeyDown);
-    window.addEventListener('hashchange', onHashChange);
-
-    self.rootNode.on('wheel', onMouseWheel);
-
-    // start presentation
-    self.transitionToFirstSlide();
-
-    function onHashChange () {
-      var hash = window.location.hash || '';
-      var viewBoxPattern = /#viewBox=(-?\d+\.?\d*),(-?\d+\.?\d*),(-?\d+\.?\d*),(-?\d+\.?\d*)/;
-
-      if (viewBoxPattern.test(hash)) {
-        var match = viewBoxPattern.exec(hash);
-        var viewBox = {
-          left: match[1],
-          top: match[2],
-          width: match[3],
-          height: match[4]
-        };
-        console.log(`Setting viewBox to ${viewBox.left},${viewBox.top},${viewBox.width},${viewBox.height}...`);
-        self.rootNode.attr('viewBox', `${viewBox.left} ${viewBox.top} ${viewBox.width} ${viewBox.height}`);
-      } else {
-        var slideId = hash.replace(/^#/, '');
-
-        var matchingSlides = self.slides.filter(function (slide) {
-          return (slide.id === slideId);
-        });
-
-        if (matchingSlides.length === 0) {
-          console.error(`No slide found with id "${slideId}"!`);
-          self.transitionToFirstSlide();
-          return;
-        }
-
-        if (matchingSlides.length > 1) {
-          console.error(`More than one slide found with id "${slideId}", using the first one!`);
-        }
-
-        self.currentSlideIndex = self.slides.indexOf(matchingSlides[0]);
-      }
-    }
-
-    function onKeyDown (event) {
-      var key = event.code || event.keyIdentifier;
-
-      var keyBinding;
-      if (self._keyBindings instanceof Map) {
-        keyBinding = self._keyBindings.get(key);
-      } else {
-        keyBinding = self._keyBindings[key];
-      }
-
-      if (keyBinding) {
-        keyBinding();
-      } else {
-        console.log(`No keybinding for ${key}`);
-      }
-    }
-
-    function onMouseWheel () {
-      var zoom;
-      if (d3.event.wheelDelta > 0) {
-        console.log('Zooming in');
-        zoom = 0.8;
-      } else {
-        console.log('Zooming out');
-        zoom = 1.25;
-      }
-
-      var oldViewBox = self.rootNode.attr('viewBox')
-        .split(' ')
-        .map(function (component) {
-          return parseInt(component);
-        });
-
-      var mouse = d3.mouse(this);
-      var viewBox = {
-        left: mouse[0] - (mouse[0] - oldViewBox[0]) * zoom,
-        top: mouse[1] - (mouse[1] - oldViewBox[1]) * zoom,
-        width: oldViewBox[2] * zoom,
-        height: oldViewBox[3] * zoom
-      };
-      setViewBox(viewBox);
-    }
-
-    function setViewBox (viewBox) {
-      window.location.hash = `#viewBox=${viewBox.left},${viewBox.top},${viewBox.width},${viewBox.height}`;
-    }
   }
 
   get currentSlideIndex () {
@@ -206,23 +70,167 @@ class SvgSlides {
     };
   }
 
+  handleEvent (event) {
+    switch (event.type) {
+      case 'click':
+        return this.onClick(event);
+      case 'hashchange':
+        return this.onHashChange(event);
+      case 'keydown':
+        return this.onKeyDown(event);
+      case 'load':
+        return this.onLoad(event);
+      case 'wheel':
+        return this.onMouseWheel(event);
+    }
+  }
+
   set keyBindings (newKeyBindings) {
     this._keyBindings = newKeyBindings;
   }
 
   static load () {
-    window.addEventListener('load', function () {
-      var svgSlides = new SvgSlides();
-      svgSlides.keyBindings = svgSlides.defaultKeyBindings;
+    var svgSlides = new SvgSlides();
+    svgSlides.keyBindings = svgSlides.defaultKeyBindings;
+    window.addEventListener('load', svgSlides);
+  }
+
+  onClick (event) {
+    var slideIndex = this.slides.indexOf(event.target);
+    if (slideIndex > -1) {
+      this.currentSlideIndex = slideIndex;
+    }
+  }
+
+  onHashChange () {
+    var hash = window.location.hash || '';
+    var slideId = hash.replace(/^#/, '');
+
+    var matchingSlides = this.slides.filter(function (slide) {
+      return (slide.id === slideId);
     });
+
+    if (matchingSlides.length === 0) {
+      console.error(`No slide found with id "${slideId}"!`);
+      this.transitionToFirstSlide();
+      return;
+    }
+
+    if (matchingSlides.length > 1) {
+      console.error(`More than one slide found with id "${slideId}", using the first one!`);
+    }
+
+    this.currentSlideIndex = this.slides.indexOf(matchingSlides[0]);
+  }
+
+  onKeyDown (event) {
+    var key = event.code || event.keyIdentifier;
+
+    var keyBinding;
+    if (this._keyBindings instanceof Map) {
+      keyBinding = this._keyBindings.get(key);
+    } else {
+      keyBinding = this._keyBindings[key];
+    }
+
+    if (keyBinding) {
+      keyBinding();
+    } else {
+      console.log(`No keybinding for ${key}`);
+    }
+  }
+
+  onLoad (event) {
+    var self = this;
+
+    console.log('Powered by d3 v' + d3.version);
+    this._rootNodeSelection = d3.select(this.options.rootNodeSelector);
+    this.rootNodeSelection.attr('preserveAspectRatio', 'xMidYMid meet');
+    this.rootNodeSelection.attr('width', '100%');
+    this.rootNodeSelection.attr('height', '100%');
+    this.rootNodeSelection.attr('zoomAndPan', 'disable'); // sadly this is not supported by all browsers, so disable it for all
+
+    this._slides = this.rootNodeSelection.selectAll(this.options.slideSelector)[0];
+
+    this._slides = this._slides.sort(function compareSlides (a, b) {
+      var sortKeyA = a[self.options.sortSlidesBy].toString();
+      var sortKeyB = b[self.options.sortSlidesBy].toString();
+      if (sortKeyA < sortKeyB) {
+        return -1;
+      } else if (sortKeyA > sortKeyB) {
+        return 1;
+      } else {
+        return 0;
+      }
+    });
+
+    if (this.slides.length > 0) {
+      var slideIds = this.slides.map(function (slide) {
+        return slide.id;
+      });
+      console.log(`Found the following slides: ${slideIds}`);
+
+      slideIds.forEach(function (slideId) {
+        var matchingSlides = self.slides.filter(function (slide) {
+          return (slide.id === slideId);
+        });
+        if (matchingSlides.length > 1) {
+          console.error(`Found duplicate slide id: ${slide.id}`);
+        }
+      });
+    } else {
+      console.error('Found no slides!');
+    }
+
+    window.addEventListener('click', this);
+    window.addEventListener('hashchange', this);
+    window.addEventListener('keydown', this);
+    window.addEventListener('wheel', this);
+
+    // start presentation
+    this.transitionToFirstSlide();
+  }
+
+  onMouseWheel (event) {
+    var mousePosition = this.rootNodeSelection.node().createSVGPoint();
+    mousePosition.x = event.clientX;
+    mousePosition.y = event.clientY;
+
+    var svgPointTransformation = this.rootNodeSelection.node().getScreenCTM().inverse();
+    mousePosition = mousePosition.matrixTransform(svgPointTransformation);
+
+    var zoom;
+    if (event.wheelDelta > 0) {
+      console.log('Zooming in');
+      zoom = 0.8;
+    } else {
+      console.log('Zooming out');
+      zoom = 1.25;
+    }
+
+    var oldViewBox = this.rootNodeSelection.attr('viewBox')
+      .split(' ')
+      .map(function (component) {
+        return parseInt(component);
+      });
+
+    var viewBox = {
+      left: mousePosition.x - (mousePosition.x - oldViewBox[0]) * zoom,
+      top: mousePosition.y - (mousePosition.y - oldViewBox[1]) * zoom,
+      width: oldViewBox[2] * zoom,
+      height: oldViewBox[3] * zoom
+    };
+
+    console.log(`Setting viewBox to ${viewBox.left},${viewBox.top},${viewBox.width},${viewBox.height}...`);
+    this.rootNodeSelection.attr('viewBox', `${viewBox.left} ${viewBox.top} ${viewBox.width} ${viewBox.height}`);
   }
 
   get options () {
     return this._options;
   }
 
-  get rootNode () {
-    return this._rootNode;
+  get rootNodeSelection () {
+    return this._rootNodeSelection;
   }
 
   get slides () {
@@ -246,8 +254,7 @@ class SvgSlides {
       height: boundingBox.height + 2 * margin.y
     };
 
-    console.log(`Setting viewBox to ${viewBox.left},${viewBox.top},${viewBox.width},${viewBox.height}...`);
-    this.rootNode.transition()
+    this.rootNodeSelection.transition()
       .attr('viewBox', `${viewBox.left} ${viewBox.top} ${viewBox.width} ${viewBox.height}`);
   }
 
@@ -265,7 +272,7 @@ class SvgSlides {
 
   transitionToOverview () {
     console.log(`Transitioning to overview..`);
-    this.transitionTo(this.rootNode.node());
+    this.transitionTo(this.rootNodeSelection.node());
   }
 
   transitionToPreviousSlide () {
